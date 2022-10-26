d3a6eef343860fbfa1b9c7fb954d8443edeeca33
tulolo287 <95403916+tulolo287@users.noreply.github.com>
2022-10-26 16:32:56 +0300

    jlj

--- src/components/Navbar/NavbarContainer.jsx
+++ src/components/Navbar/NavbarContainer.jsx
@@ -1,7 +1,12 @@
+import { connect } from "react-redux";
+import Navbar from "./index";
 
+const mapToState = (state) => {
+  return {
+    navs: state.profileReducer.navs
+  };
+};
 
-const NavbarContainer = (state) {
+const NavbarContainer = connect(mapToState)(Navbar);
 
-}
-
-export default NavbarContainer
+export default NavbarContainer;
